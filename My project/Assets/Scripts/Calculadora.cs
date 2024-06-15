63c76186399fb6fd6a8286898e75c6062f354d3d
Alex-OrteCana <alejo.ortegacanales@gmail.com>
2024-06-15 16:34:35 -0600

    Calculadora

--- My project/Assets/Scripts/Calculadora.cs
+++ My project/Assets/Scripts/Calculadora.cs
@@ -1,11 +1,65 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro; 
 
 public class Calculadora : MonoBehaviour
 {
     // Variables
 
+    public TMP_InputField primerNumero; 
+    public TMP_InputField segundoNumero;
+    public TMP_Text resultadoText;
+    public int resultado;
 
     //Metodos
+
+    public void Suma()
+    {
+        int numero1 = int.Parse(primerNumero.text);
+        int numero2 = int.Parse(segundoNumero.text);
+
+        resultado = numero1 + numero2; 
+
+        resultadoText.text = resultado.ToString();
+
+
+    }
+
+    public void Resta()
+    {
+        int numero1 = int.Parse(primerNumero.text);
+        int numero2 = int.Parse(segundoNumero.text);
+
+        resultado = numero1 - numero2;
+
+        resultadoText.text = resultado.ToString();
+
+
+    }
+
+    public void Multiplicar()
+    {
+        int numero1 = int.Parse(primerNumero.text);
+        int numero2 = int.Parse(segundoNumero.text);
+
+        resultado = numero1 * numero2;
+
+        resultadoText.text = resultado.ToString();
+
+
+    }
+
+    public void Dividir()
+    {
+        int numero1 = int.Parse(primerNumero.text);
+        int numero2 = int.Parse(segundoNumero.text);
+
+        resultado = numero1 / numero2;
+
+        resultadoText.text = resultado.ToString();
+
+
+    }
+
 }
