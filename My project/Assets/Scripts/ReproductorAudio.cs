6432ac8aced8538052e8a05025c8cad78f260699
Alex-OrteCana <alejo.ortegacanales@gmail.com>
2024-06-28 17:43:46 -0600

    AudioPlayer_Terminado

---
+++ My project/Assets/Scripts/ReproductorAudio.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ReproductorAudio : MonoBehaviour
+{
+    public AudioSource repAudio;
+    public List<AudioClip> album_01 = new List<AudioClip>();
+    public Transform trackContainer;
+    public GameObject trackObj;
+    public int currentTrack;
+
+    void Start()
+    {
+        for (int i = 0; i < album_01.Count; i++)
+        {
+            Instantiate(trackObj, trackContainer);
+            trackObj.GetComponent<Track>().clip = album_01[i];
+            trackObj.GetComponent<Track>().idTrack = i;
+
+        }
+    }
+
+}
